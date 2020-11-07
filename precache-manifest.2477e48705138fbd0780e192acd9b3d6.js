e94d5487ba5d534e674076be095d1c96d42bd554
Dev Singh <dev@devksingh.com>
2020-11-07 12:26:35 -0600

    Updates

---
+++ precache-manifest.2477e48705138fbd0780e192acd9b3d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e758c9c42e525750c8e89d7631ad277",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c24d4b42ad651840466",
+    "url": "./static/js/2.270c3fa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.270c3fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9eeb3f851e8a598f1a",
+    "url": "./static/js/main.c0b09345.chunk.js"
+  },
+  {
+    "revision": "cfd5d991e36f8dff2e74",
+    "url": "./static/js/runtime-main.cc838eab.js"
+  }
+]);
