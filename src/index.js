cbda0d5cafa98d0e3b9b6a11cebffd1d6db7c21a
Dev Singh <dsingh@imsa.edu>
2021-01-18 10:50:45 -0600

    add version indicator

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import Signinpage from './signinpage'
 
 ReactDOM.render(
-  <Signinpage />
+  <div>
+    <p>v0.1.0</p>
+    <Signinpage />
+  </div>
 ,document.getElementById('root')
 );
